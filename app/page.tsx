fe61037770a773305fb58528ee70232509a13ec5
Ktjj91 <kingsleythomas33@gmail.com>
2024-08-12 20:18:57 +0200

    deuxieme commit

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const getTasks = async () => {
             }
             ,headers:{
                 "Content-Type": "application/json",
-            }
+            },
         });
         return response.json();
     } catch (e:any) {
--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -6,9 +6,13 @@ export default function TaskList({rows,deleteTask,updateTask}:any){
 
     return (
         <section className="mt-5 flex flex-col gap-4 justify-center items-center">
-            {rows.map((task:TaskInterface) => (
-                <Task key={task.id} task={task} deleteTask={deleteTask} updateTask={updateTask} />
-            ))}
+            {
+                rows &&
+                rows.map((task:TaskInterface) => (
+                        <Task key={task.id} task={task} deleteTask={deleteTask} updateTask={updateTask} />
+                    ))
+            }
+
         </section>
     )
 }
